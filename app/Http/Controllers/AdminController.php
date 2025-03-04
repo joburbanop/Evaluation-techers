83b031c55ea81a944241a832bc1d764abae17136
joburbanop <joburbanop589@gmail.com>
2025-03-04 16:42:59 -0500

    cambios iniciales

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,10 +9,10 @@ class AdminController extends Controller
 {
     public function index()
     {
-        // Obtén todos los usuarios (asegúrate de que tienes datos en la base)
+        
         $users = User::all();
         
-        // Pasa los usuarios a la vista
+  
         return view('admin.index', compact('users'));
     }
 }
